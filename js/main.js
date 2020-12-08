97df7b383cda3fa19a8419a0d202cd3cafc374f9
MiguelCotarelo <cotish23@gmail.com>
2020-12-07 21:18:31 -0600

    First commit JS

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+console.log('Hola Koders')
+
+alert('Hola Koders')
+
+// Variables
+// let, var y const. Las más usadas son let y var
+// var sirve para declarar variables. El problema es que se declara de manera global
+// let sirve para declarar. La ventaja es que las variables sólo vab con Java, y son "locales"
+// const declara variables globales. Se declara con mayúsculas de preferencia. Se escriben como snake_case con mayúsculas
+ 
+
+var nombreVariable;
+let nombreVariable2
+
+let nombreVariable3 = 'Algo' //cadena 
+let numero = 1000 // valor numérico
+let boleano = true // o false
+let myArray = [1, '1', true] // Array
+let myObjectKoder = { // Objetos
+    nombre: 'Mike',
+}
+
+// Formas en definir variables
+// Case Styles
+// PascalCase
+// snake_case
+// kebab-case --> Es la menos recomendable por el guion medio
+
