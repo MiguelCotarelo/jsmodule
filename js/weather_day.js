879a91b8ada8e6d49ee6654a200cec2075d40bd8
MiguelCotarelo <cotish23@gmail.com>
2020-12-09 08:57:07 -0600

    ADD Switch and if else class two

--- js/weather_day.js
+++ js/weather_day.js
@@ -4,22 +4,43 @@ console.log('Estado del clima')
 // let lluvioso
 // let nevado
 // let nublado
-const WEATHER = prompt(`Dime como está el clima: soleado, lluvioso, nevado, nublado u otro`)
+const WEATHER = prompt(`Dime como está el clima: soleado, lluvioso, nevado, nublado`)
 
 
-if (WEATHER === 'soleado') {
-    console.log(`El calor está de locos`)
+// ejercicio con estructura if
+// if (WEATHER === 'soleado') {
+//     console.log(`El calor está de locos`)
 
-} else if (WEATHER === 'lluvioso') {
-    console.log(`Espero que traigas paragüas`)
+// } else if (WEATHER === 'lluvioso') {
+//     console.log(`Espero que traigas paragüas`)
 
-} else if (WEATHER === 'nevado') {
-    console.log(`Una chamarra no te caería mal`)
+// } else if (WEATHER === 'nevado') {
+//     console.log(`Una chamarra no te caería mal`)
 
-} else if (WEATHER === 'nublado') {
-    console.log(`Que delicia de clima`)
+// } else if (WEATHER === 'nublado') {
+//     console.log(`Que delicia de clima`)
 
-} else {
-    const WEATHER_DESCRIPTION = prompt('Dame una descripción del clima')
-    console.log(WEATHER_DESCRIPTION)
+// } else {
+//     const WEATHER_DESCRIPTION = prompt('Dame una descripción del clima')
+//     console.log(WEATHER_DESCRIPTION)
+// }
+
+
+//ejercicio con estructura switch
+switch(WEATHER){
+    case 'soleado':
+        console.log(`El calor está de locos`)
+    break
+    case 'lluvioso':
+        console.log(`Espero que traigas paragüas`)
+    break
+    case 'nevado':
+        console.log(`Una chamarra no te caería mal`)
+    break
+    case 'nublado':
+        console.log(`Que delicia de clima`)
+    break
+    default:
+        const WEATHER_DESCRIPTION = prompt('Dame una descripción del clima')
+        console.log(WEATHER_DESCRIPTION)
 }
