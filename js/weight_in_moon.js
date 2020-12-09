b9f55be6b7256537bc7fed309d39b5cc0817bee4
MiguelCotarelo <cotish23@gmail.com>
2020-12-08 19:51:43 -0600

    Add excercise moon

---
+++ js/weight_in_moon.js
@@ -0,0 +1,13 @@
+// 1. Estar en la rama class/class-one
+// 2. Crear el archivo weight_in_moon.js
+// En el archivo...
+// pedir al usuario su peso en KG
+// 4. arrojar en un alert() cual sería su peso en la luna
+
+let weigth_in_earth = prompt('¿Cual es tu peso en KG')
+let weight_in_moon = (weight_in_earth/9.31) * 1.622
+console.log(weight_in_moon)
+let result = 'Tu peso en la luna es: ' + weight_in_moon + ' Kg'
+alert (result)
+
+//Termina el ejercicio, porque algo está tronando -.-
