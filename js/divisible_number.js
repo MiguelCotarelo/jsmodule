637088c3c7e9f1cf98e28dfe0f38537498bd6423
MiguelCotarelo <cotish23@gmail.com>
2020-12-09 15:44:41 -0600

    se agregan cambios

--- js/divisible_number.js
+++ js/divisible_number.js
@@ -2,3 +2,10 @@
 // pedir al usuario por prompt un numero entre 1 y 100
 // comparar si ese numero es divisible entre 2 y 11 e imprimir en consola un mensaje indicandolo
 // sino, enviar un mensaje negandolo
+console.log('Saber si un número es divisible entre 2 y 11')
+const DIVISIBLE = prompt(`Dame un número entre 1 y 100`)
+if (DIVISIBLE ) {
+    
+} else {
+    
+}
