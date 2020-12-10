9c044dddb486901c1b8e833d10f9e88becd72480
MiguelCotarelo <cotish23@gmail.com>
2020-12-09 22:17:48 -0600

    Add clase 3

--- js/string_array.js
+++ js/string_array.js
@@ -1,57 +1,57 @@
 /** 
- * ejercicio 1
-    1. Asignar un string a una variable 
-    2. Verificar cuantos caracteres tiene ese string
-    3. Imprimirlo en consola
+* ejercicio
+1. Asignar un string a una variable 
+2. Verificar cuantos caracteres tiene ese string
+3. Imprimirlo en consola
 */
 
-let str ='PALABRA';
+let str = 'PALABRA';
 console.log(`la palabra "${str}" tiene  ${str.length} caracteres`);
 
 /**
- * Ejercicio 2
- * Declarar 2 variables con strings y concatenarlos
- */
+* Ejercicio 2
+* Declarar 2 variables con strings y concatenarlos
+*/
 
- let str1 ='variable1';
- let str2 = 'variable2';
+let str1 = 'variable1';
+let str2 = 'variable2';
 
-console.log(str1.concat(' ',str2));
-
-
-/**
- * Ejercicio 3
- * Declarar un string de al menos 10 caracteres
- * imprimir en consola del 4 al 6 caracter
- */
-
- const CARACTER = 'EJERCICIO TRES';
- console.log(CARACTER.slice(3,6))
+console.log(str1.concat(' ', str2));
 
 
 /**
- * Ejercicio 4
- * Declarar un string de al menos 2 palabras
- * imprimir en consola el string separado por guiones en cada palabra
- * "hola koders"
- * "hola-koders"
- */
+* Ejercicio 3
+* Declarar un string de al menos 10 caracteres
+* imprimir en consola del 4 al 6 caracter
+*/
 
- const PALABRAS = 'UNO DOS TRES CUATRO';
- console.log(PALABRAS.replaceAll(' ', '-'))
+const CARACTER = 'EJERCICIO TRES';
+console.log(CARACTER.slice(3, 6))
 
 
- /**
- * Ejercicio 5
- * Declarar un string de al menos 2 palabras
- * imprimir en consola el string con cada una de las palabras Capitalizadas
- * "hola koders"
- * "Hola Koders"
- */
+/**
+* Ejercicio 4
+* Declarar un string de al menos 2 palabras
+* imprimir en consola el string separado por guiones en cada palabra
+* "hola koders"
+* "hola-koders"
+*/
+
+const PALABRAS = 'UNO DOS TRES CUATRO';
+console.log(PALABRAS.replaceAll(' ', '-'))
+
+
+/**
+* Ejercicio 5
+* Declarar un string de al menos 2 palabras
+* imprimir en consola el string con cada una de las palabras Capitalizadas
+* "hola koders"
+* "Hola Koders"
+*/
 
 const PALABRASEJ5 = 'uno dos';
 const ARREGLO = PALABRASEJ5.split(' ');
-let arregloCapitalizado= [];
+let arregloCapitalizado = [];
 ARREGLO.forEach(palabra => {
     arregloCapitalizado.push(palabra.charAt(0).toUpperCase() + palabra.slice(1))
 });
@@ -59,15 +59,19 @@ ARREGLO.forEach(palabra => {
 console.log(arregloCapitalizado.join().replaceAll(',', ' '))
 
 /**
- * Ejercicio 6
- * Declarar un string con un correo electrónico
- * Enmascarar con "*" el bloque del usuario
- * "jorgec@kodemia.mx"
- * "*****@kodemia.mx"
- */
+* Ejercicio 6
+* Declarar un string con un correo electrónico
+* Enmascarar con "*" el bloque del usuario
+* "jorgec@kodemia.mx"
+* "*****@kodemia.mx"
+*/
 
- const usuario = 'jorgec@kodemia.mx';
- console.log(usuario.replace('jorgec', '*****'));
+const usuario = 'jorgec@kodemia.mx';
+console.log(usuario.replace('jorgec', '*****'));
+// Se puede hacer con indexOf()
+
+
+
 
 /*Ejercicio 8
 Declarar un string de al menos 2 palabras
