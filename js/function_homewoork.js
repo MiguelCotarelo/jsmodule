3b917593a11633d319445b099589658b0ee35ccc
MiguelCotarelo <cotish23@gmail.com>
2020-12-11 08:33:16 -0600

    commit de functions

---
+++ js/function_homewoork.js
@@ -0,0 +1,17 @@
+function myFunction (){
+    //sentencias
+    console.log('Mi primera función')
+}
+
+function getName(){
+    const name = prompt(`¿Cuál es tu nombre?`)
+    console.log(name)
+}
+
+function nameReverse (stringName){
+    console.log(stringName)
+}
+
+function add (valOne, valTwo){
+    console.log(valOne + valTwo)
+}
